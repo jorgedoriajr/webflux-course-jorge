86deddfbaac9eb609af68e5f0d198f4ee55a5e72
jorgedoriajr <jorgedoriajr@gmail.com>
2024-06-04 00:06:52 -0300

    start project

---
+++ src/test/java/com/example/jorgedoria/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jorgedoria.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
